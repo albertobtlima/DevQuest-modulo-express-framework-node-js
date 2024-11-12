8f87e8d1658956826c7b6aa5b56b69542849b928
Alberto Lima <alberto.btlima@outlook.com>
2024-11-12 20:37:05 -0300

    add: arquivos-estaticos

---
+++ 10-arquivos-estaticos/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const publicDirectoryPath = path.join(__dirname, "public");
+console.log(__dirname);
+console.log(publicDirectoryPath);
+
+app.use(express.static(publicDirectoryPath));
+app.use("/assets", express.static("./files"));
+
+// Inicia o servidor
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta http://localhost:3000");
+});
