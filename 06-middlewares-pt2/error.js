22c0c3014eff839f4f38036718a79747260d66d5
Alberto Lima <alberto.btlima@outlook.com>
2024-11-12 18:32:53 -0300

    add use on error

--- 06-middlewares-pt2/error.js
+++ 06-middlewares-pt2/error.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   throw new Error("Do not funciona!");
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send("Ocorreu um erro");
+});
+
 app.listen(3000, () => {
   console.log("Servidor iniciado na porta http://localhost:3000");
 });
